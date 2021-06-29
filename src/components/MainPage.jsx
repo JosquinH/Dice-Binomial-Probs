5b588ad85b462a97dc5fd0c48268090e24e703c9
JosquinH <josquin.havard@orange.fr>
2021-06-29 23:18:33 +0200

    :sparkles: update Layout

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -13,6 +13,8 @@ const useStyles = makeStyles((theme) => ({
     box: {
         margin: '2%',
         alignSelf: 'center',
+        marginBottom: "20px",
+        flex: 1
     }
 }))
 
@@ -30,17 +32,17 @@ const MainPage = (props) => {
                 <Box className={classes.box}>
                     <Form formId={formId} onSubmit={handleOnSubmit} />
                 </Box>
-                <Box style={{flex: 1, alignItems: 'center'}} className={classes.box}>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    startIcon={<Calculator />}
-                    type='submit'
-                    fullWidth={false}
-                    form={formId}
-                > 
-                    Calculer les probabilités
-                </Button>
+                <Box style={{ alignItems: 'center' }} className={classes.box}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        startIcon={<Calculator />}
+                        type='submit'
+                        fullWidth={false}
+                        form={formId}
+                    >
+                        Calculer les probabilités
+                    </Button>
                 </Box>
             </Paper>
         </Container>
